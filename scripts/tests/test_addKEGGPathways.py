30a3720b023b0e7ed1e53062957d4bfa92e65ead
Isidora Beslic <122387780+DoraBeslic@users.noreply.github.com>
2024-07-09 14:03:20 -0400

    Update test_addKEGGPathways.py

--- scripts/tests/test_addKEGGPathways.py
+++ scripts/tests/test_addKEGGPathways.py
@@ -9,12 +9,12 @@ from addKEGGPathways import getKeggPathIDs
 
 def test_getUniProtFromblast_belowThreshold():
     """Return UniProt ID from BLAST line."""
-    blast_line = "TRINITY_DN21412_c0_g1_i1.p1     Q66HC5  111     819     111     69      62.162  1.25e-37        RecName: Full=Nuclear pore complex protein Nup93; AltName: Full=93 kDa nucleoporin; AltName: Full=Nucleoporin Nup93"
+    blast_line = "TRINITY_DN21412_c0_g1_i1.p1     Q66HC5  111     819     69      62.162  1.25e-37        RecName: Full=Nuclear pore complex protein Nup93; AltName: Full=93 kDa nucleoporin; AltName: Full=Nucleoporin Nup93"
     assert getUniProtFromBlast(blast_line, "1e-30") == "Q66HC5", "Expect the UniProt ID"
 
 def test_getUniProtFromblast_aboveThreshold():
     """Return False because e-value doesn't pass the threshold."""
-    blast_line = "TRINITY_DN21412_c0_g1_i1.p1     Q66HC5  111     819     111     69      62.162  1.25e-37        RecName: Full=Nuclear pore complex protein Nup93; AltName: Full=93 kDa nucleoporin; AltName: Full=Nucleoporin Nup93"
+    blast_line = "TRINITY_DN21412_c0_g1_i1.p1     Q66HC5  111     819     69      62.162  1.25e-37        RecName: Full=Nuclear pore complex protein Nup93; AltName: Full=93 kDa nucleoporin; AltName: Full=Nucleoporin Nup93"
     assert getUniProtFromBlast(blast_line, "1e-40") == False, "Expect the e-value threshold to fail"
 
 def test_getKeggGenes():
